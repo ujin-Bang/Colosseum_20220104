ad3afeba113f787a24033d1eee7c5c9c7209db1e
ujin-Bang <qkddnwls14@gmail.com>
2022-01-05 22:08:03 +0900

    연습 - "토큰을 첨부"해서, 메인화면에서 내 정보 받아서 텍스트뷰에 반영

--- app/src/main/java/com/neppplus/colosseum_20220104/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/colosseum_20220104/utils/ServerUtil.kt
@@ -1,5 +1,6 @@
 package com.neppplus.colosseum_20220104.utils
 
+import android.content.Context
 import android.util.Log
 import okhttp3.*
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
@@ -75,7 +76,7 @@ class ServerUtil {
 
         }
 
-//  회원가입 함수 - PUT
+//          회원가입 함수 - PUT
         fun putRequestSignUp(email: String, pw: String, nickname: String, handler: JsonResponseHandler?){
 
             val urlString = "${HOST_URL}/user"
@@ -149,6 +150,48 @@ class ServerUtil {
 
                 override fun onResponse(call: Call, response: Response) {
                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+                }
+
+
+            })
+
+        }
+
+//        연습 - 내 정보 가져오기 (토큰 첨부) -GET
+        fun getRequestMyInfo( context: Context, handler: JsonResponseHandler? ) {
+
+            val urlBuilder = "${HOST_URL}/user_info".toHttpUrlOrNull()!!.newBuilder() // 서버 주소 /기능주소까지만작성
+//            urlBuilder.addEncodedQueryParameter("type",type)
+//            urlBuilder.addEncodedQueryParameter("value", value)
+
+
+            val urlString = urlBuilder.toString()
+
+            Log.d("완성주소",urlString)
+
+//      Request를 만들 때 헤더도 같이 첨부.
+
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-Token", ContextUtil.getToken(context))
+                .build()
+
+
+//            실제 API 호출 - client
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue( object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
                     val jsonObj = JSONObject(bodyString)
                     Log.d("서버응답", jsonObj.toString())
                     handler?.onResponse(jsonObj)
