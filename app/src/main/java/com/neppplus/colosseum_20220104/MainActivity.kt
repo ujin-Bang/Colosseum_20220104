1dc22f48f32bb0e281e3c7e4de67fceeeb0f13a5
ujin-Bang <qkddnwls14@gmail.com>
2022-01-06 12:16:49 +0900

    토론 목록 list item xml 까지 작성

--- app/src/main/java/com/neppplus/colosseum_20220104/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20220104/MainActivity.kt
@@ -25,31 +25,33 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
 //        연습 - 내 정보 API 호출 -> 닉네임 추출 /UI 반영
-        getMyInfoFromServer()
+//        getMyInfoFromServer()
+
+//        실제 - 메인 화면의 데이터 받아오기 -> 토론주제 목록 -> 리스트뷰로 표기
 
     }
 
-    fun getMyInfoFromServer(){
-
-        ServerUtil.getRequestMyInfo(mContext, object :ServerUtil.JsonResponseHandler{
-            override fun onResponse(jsonObj: JSONObject) {
-
-                val dataObj = jsonObj.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname =userObj.getString("nick_name")
-
-                runOnUiThread {
-
-                    binding.txtUserNickname.text = nickname
-
-                }
-
-            }
-
-
-        })
-
-    }
+//    fun getMyInfoFromServer(){
+//
+//        ServerUtil.getRequestMyInfo(mContext, object :ServerUtil.JsonResponseHandler{
+//            override fun onResponse(jsonObj: JSONObject) {
+//
+//                val dataObj = jsonObj.getJSONObject("data")
+//                val userObj = dataObj.getJSONObject("user")
+//                val nickname =userObj.getString("nick_name")
+//
+//                runOnUiThread {
+//
+//                    binding.txtUserNickname.text = nickname
+//
+//                }
+//
+//            }
+//
+//
+//        })
+//
+//    }
 
 
 }
