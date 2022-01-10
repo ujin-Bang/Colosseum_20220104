48a62ddb4008c90952b83132a75377d6164cce94
ujin-Bang <qkddnwls14@gmail.com>
2022-01-10 16:55:58 +0900

    댓글 내용 실제 표시

--- app/src/main/java/com/neppplus/colosseum_20220104/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/neppplus/colosseum_20220104/adapters/ReplyAdapter.kt
@@ -29,6 +29,10 @@ class ReplyAdapter(
 
         val data = mList[position]
 
+        val txtReplyContent = row.findViewById<TextView>(R.id.txtReplyContent)
+
+        txtReplyContent.text = data.content
+
 
 
         return row
