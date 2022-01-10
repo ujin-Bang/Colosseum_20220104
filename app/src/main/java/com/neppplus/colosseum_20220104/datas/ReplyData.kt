11b0937b7ba1d152752ea265a5688e074a40968f
ujin-Bang <qkddnwls14@gmail.com>
2022-01-10 18:00:25 +0900

    댓글 - 선택 진영 파싱 / 표시

--- app/src/main/java/com/neppplus/colosseum_20220104/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_20220104/datas/ReplyData.kt
@@ -9,6 +9,9 @@ class ReplyData {
 
     var writer = UserData() //사용자 데이터가 들어올 거라고 명시.
 
+//    어느 진영에 대한 댓글?
+    var selectedSide = SideData()
+
     companion object {
 
         fun getReplyDataFromJson(jsonObj: JSONObject): ReplyData {
@@ -21,6 +24,9 @@ class ReplyData {
 
             val userObj = jsonObj.getJSONObject("user")
             replyData.writer = UserData.getUserDataFromJson(userObj)
+
+            val sideObj = jsonObj.getJSONObject("selected_side")
+            replyData.selectedSide = SideData.getSideDataFromJson(sideObj)
 
             return replyData
         }
