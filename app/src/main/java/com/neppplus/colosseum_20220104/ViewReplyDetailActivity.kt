cd048c7ec7a2127581d3c420d154cdc86f0de3c0
ujin-Bang <qkddnwls14@gmail.com>
2022-01-12 17:47:28 +0900

    댓글 상세보기 화면 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20220104/ViewReplyDetailActivity.kt
@@ -0,0 +1,27 @@
+package com.neppplus.colosseum_20220104
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20220104.databinding.ActivityViewReplyDetailBinding
+
+class ViewReplyDetailActivity : BaseActivity() {
+
+    lateinit var binding : ActivityViewReplyDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      binding = DataBindingUtil.setContentView(this,R.layout.activity_view_reply_detail)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
