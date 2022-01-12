e1c1a8c7ba125c46b7d161874ab8ef78bae4cce8
ujin-Bang <qkddnwls14@gmail.com>
2022-01-12 17:57:09 +0900

    어댑터에서 화면 진입  - mContext의 startActivity기능 활용

--- app/src/main/java/com/neppplus/colosseum_20220104/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/neppplus/colosseum_20220104/adapters/ReplyAdapter.kt
@@ -1,6 +1,7 @@
 package com.neppplus.colosseum_20220104.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.neppplus.colosseum_20220104.R
+import com.neppplus.colosseum_20220104.ViewReplyDetailActivity
 import com.neppplus.colosseum_20220104.ViewTopicDetailActivity
 import com.neppplus.colosseum_20220104.datas.ReplyData
 import com.neppplus.colosseum_20220104.datas.TopicData
@@ -98,6 +100,12 @@ class ReplyAdapter(
                 }
 
             })
+
+        }
+        txtReplyCount.setOnClickListener {
+
+            val myIntent = Intent(mContext, ViewReplyDetailActivity::class.java)
+            mContext.startActivity(myIntent)
 
         }
 
